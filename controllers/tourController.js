87c429a0cb2ba397407792b1f2386fedc92023ae
Winston Zhou <zhouwinston1@gmail.com>
2025-01-08 19:23:41 -0500

    refactoring for MVC

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,18 +1,4 @@
-const fs = require('fs');
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
-exports.checkID = (req, res, next, val) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-  next();
-};
+const Tour = require('./../models/tourModel');
 
 exports.checkBody = (req, res, next) => {
   console.log(req.body);
@@ -31,9 +17,9 @@ exports.getAllTours = (req, res) => {
     status: 'success',
     requestedAt: req.requestTime,
     results: tours.length,
-    data: {
-      tours,
-    },
+    // data: {
+    //   tours,
+    // },
   });
 };
 
@@ -41,35 +27,23 @@ exports.getTour = (req, res) => {
   console.log(req.params);
 
   const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
+  //   const tour = tours.find((el) => el.id === id);
 
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
+  //   res.status(200).json({
+  //     status: 'success',
+  //     data: {
+  //       tour,
+  //     },
+  //   });
 };
 
 exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  console.log('heyll');
-
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/../dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
+  res.status(201).json({
+    status: 'success',
+    data: {
+      tour: newTour,
+    },
+  });
 };
 
 exports.updateTour = (req, res) => {
--- server.js
+++ server.js
@@ -1,6 +1,17 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose.connect(DB, {}).then((con) => {
+  console.log(con.connections);
+  console.log('DB connection successful!');
+});
 
 const app = require('./app');
 
